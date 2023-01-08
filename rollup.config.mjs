e4cfbc7b4829e18f3bfc848ca1f70a5319cf1373
Adam Wolf <adamwolf@feelslikeburning.com>
2023-01-08 12:55:49 -0600

    Update dependencies and configs
    I updated the dependencies. I migrated from rollup-plugin-terser to
    @rollup/plugin-terser.
    
    I changed from testing on Node 12.x and 14.x with GitHub Actions to
    testing with Node 14.x, 16.x, and 18.x.  I updated the GitHub Action
    action versions, too.
    
    I moved the rollup config to rollup.config.mjs per a warning.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs"
 import babel from "@rollup/plugin-babel"
 import resolve from "@rollup/plugin-node-resolve"
 import json from "@rollup/plugin-json"
-import { terser } from "rollup-plugin-terser"
+import terser from "@rollup/plugin-terser"
 
 export default {
   input: "src/index.ts",
